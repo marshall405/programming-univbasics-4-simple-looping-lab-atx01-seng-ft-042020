6cd73b7ea2b3c94be16a4532c90f078f547995fb
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-03 07:02:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,12 @@ def output_array(array)
    counter += 1
  end
 end
+
+def return_string_array(array)
+  counter = 0
+  new_array = []
+  while array[counter] do
+    new_array << array[counter].to_s 
+  end
+  new_array
+end
