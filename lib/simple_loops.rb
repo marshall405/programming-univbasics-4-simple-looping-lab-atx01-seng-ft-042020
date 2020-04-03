433b6f566dc0b73846d87ea9d4c2712f18365eeb
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-03 06:58:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0 
+  while counter < 5 do
+    puts message
+  end
+end
+
+def loop_message_n_times(message,n)
+  counter = 0 
+  while counter < n do 
+    puts message
+  end
+end
+
+def output_array(array)
+ counter = 0 
+ while array[counter] do
+   puts array[counter]
+ end
+end
